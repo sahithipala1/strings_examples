5c88ac4c9c65c470efe7a6de96af4e719fae482d
sahithipala1 <sahithipala1@gmail.com>
2022-01-13 12:00:49 -0500

    new

---
+++ string_operation_2.py
@@ -0,0 +1,26 @@
+"""
+Time to join!
+
+While normalizing your text, you noticed that one
+ review had a particular structure. This review
+ ends with the HTML tag <\i> and it has a lot  of
+  commas in different places of the sentence.
+  You decide to remove the tag from the end and use the strategy of
+   splitting the string and joining it back again without the commas.
+
+"""
+movie = " the film,however,is all good<\i>"
+
+
+movie_tag = movie.rstrip("<\i>")
+
+print(movie_tag)
+
+movie_no_comma = movie_tag.split(",")
+
+print(movie_no_comma)
+
+
+movie_join = " ".join(movie_no_comma)
+
+print(movie_join)
