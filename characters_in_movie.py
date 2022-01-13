d02adc7b7cb940dbf6294fad3b214e8b2b4d183f
sahithipala1 <sahithipala1@gmail.com>
2022-01-13 07:57:01 -0500

    strings

---
+++ characters_in_movie.py
@@ -0,0 +1,18 @@
+"""
+The text of one movie review has been already saved in the variable movie.
+ You can use print(movie) to view the variable in the IPython Shell.
+
+"""
+
+movie = "fox and kelley soon become bitter rivals because the new fox books store is opening up right across the block " \
+        " from the small business."
+
+print(movie)
+
+# find the characters in movie variable
+
+length_of_movie = len(movie)
+
+to_string = str(length_of_movie)
+
+print("Number of characters in the movie review :" + " " + to_string)
